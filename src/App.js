a94a69168ab2c92f7516cdff4068b55e5f24ec55
imlimge <limge12@gmail.com>
2023-11-08 12:00:32 +0900

    1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let isrc =`https://i.imgur.com/LDNMbNK.gif`
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <img src={isrc} style={{borderRadius:'50%'}} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         지금은 23 11 08 / 11시 51분
         </a>
       </header>
     </div>
